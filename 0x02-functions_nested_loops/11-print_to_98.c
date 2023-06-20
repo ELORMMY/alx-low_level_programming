4a43abca4f972466d253d23f84e4089b1f8feb42
ELORMMY <hennyelormmy@gmail.com>
2023-06-20 16:30:22 -0700

    11 correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,24 +1,33 @@
+
 #include <stdio.h>
-
+#include "main.h"
 /**
- * main - Lists all the natural numbers below 1024 (excluded)
- *        that are multiples of 3 or 5.
- *
- * Return: Always 0.
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
  */
-int main(void)
+void print_to_98(int n)
 {
-	int i, sum = 0;
+	int i, j;
 
-	for (i = 0; i < 1024; i++)
+
+	if (n <= 98)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-		sum += i;
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			printf("%d, ", i);
+			else if (i == 98)
+			printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+			printf("%d, ", j);
+			else if (j == 98)
+			printf("%d\n", j);
+		}
 	}
-
-
-	printf("%d\n", sum);
-
-
-	return (0);
 }
